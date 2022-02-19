aa99dc6ef60259afdbcbf35b2c30a7a06e8ce0b6
Zhao Yi <zhao.yi@ericsson.com>
2022-02-19 17:14:43 +0200

    Initialization

---
+++ src/test/java/com/test/countryClient/CountryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.countryClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
